0aa9571ed1fb772b291deba0eb1ddc8f99804a13
Christopher Loessl <cloessl@x-berg.de>
2013-11-20 02:56:40 +0100

    Login works

--- Bucketify/Bucketify/ViewController.h
+++ Bucketify/Bucketify/ViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CocoaLibSpotify.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <SPSessionDelegate>
 
 @end
--- Bucketify/Bucketify/ViewController.m
+++ Bucketify/Bucketify/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#include "appkey.c"
+
 @interface ViewController ()
 
 @end
@@ -18,12 +20,70 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSError *error = nil;
+    [SPSession initializeSharedSessionWithApplicationKey:[NSData dataWithBytes:&g_appkey length:g_appkey_size]
+                                               userAgent:@"org.loessl.Bucketify-iOS"
+                                           loadingPolicy:SPAsyncLoadingManual
+                                                   error:&error];
+    
+    if (error != nil) {
+        NSLog(@"CocoaLibSpotify init failed: %@", error);
+        abort();
+    }
+    
+    [[SPSession sharedSession] setDelegate:self];
+    
+    // make sure that self.viewController is attached or placed in window hierarchy
+    [self performSelector:@selector(login) withObject:nil afterDelay:0.0];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - login
+
+-(void)login
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSMutableDictionary *storedCredentials = [defaults valueForKey:@"SpotifyUsers"];
+    
+    if (storedCredentials == nil) {
+        [self showLogin];
+    } else {
+        NSString *lastUser = [storedCredentials objectForKey:@"LastUser"] ;
+        [[SPSession sharedSession] attemptLoginWithUserName:lastUser existingCredential:[storedCredentials objectForKey:lastUser]];
+    }
+}
+
+-(void)showLogin
+{
+    SPLoginViewController *controller = [SPLoginViewController loginControllerForSession:[SPSession sharedSession]];
+    controller.allowsCancel = NO;
+    
+    if (controller) {
+        [self presentViewController:controller animated:NO completion:nil];
+    } else {
+        NSLog(@"Login window can't be shown");
+    }
+}
+
+- (void)session:(SPSession *)aSession didGenerateLoginCredentials:(NSString *)credential forUserName:(NSString *)userName
+{
+    NSLog(@"storing credentials");
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSMutableDictionary *storedCredentials = [[defaults valueForKey:@"SpotifyUsers"] mutableCopy];
+    
+    if (storedCredentials == nil)
+        storedCredentials = [NSMutableDictionary dictionary];
+    
+    [storedCredentials setValue:credential forKey:userName];
+    [storedCredentials setValue:userName forKey:@"LastUser"];
+    [defaults setValue:storedCredentials forKey:@"SpotifyUsers"];
+    [defaults synchronize];
 }
 
 @end
